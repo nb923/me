d40f8246177a69789cd7057c3a545acbd36028c5
nb923 <nb923@scarletmail.rutgers.edu>
2025-07-29 12:06:22 -0400

    Add basic agent input page

--- my-page/src/App.jsx
+++ my-page/src/App.jsx
@@ -6,11 +6,13 @@ import { AuroraText } from "@/components/magicui/aurora-text";
 import { AnimatePresence, motion } from "framer-motion";
 import { TextAnimate } from "@/components/magicui/text-animate";
 import { Pointer } from "@/components/magicui/pointer";
-
+import { Textarea } from "./components/ui/textarea";
+import { Send, File as LucideFile } from "lucide-react";
+import { Button } from "./components/ui/button";
 
 function App() {
   const [isIntroVisible, setIsIntroVisible] = useState(true);
-  const words = ["Nideesh", "Website"];
+  const words = ["Nideesh", "GPT"];
 
   const containerVariants = {
     hidden: {},
@@ -51,12 +53,12 @@ function App() {
                 <motion.span
                   layout
                   key={idx}
-                  className="inline-block mr-4"
+                  className="inline-block mr-2"
                   variants={wordVariants}
                   transition={{ duration: 0.5, ease: "easeIn" }}
                 >
-                  {word === "Website" ? (
-                    <AuroraText>Website</AuroraText>
+                  {word === "GPT" ? (
+                    <AuroraText>{word}</AuroraText>
                   ) : (
                     <p>{word}</p>
                   )}
@@ -65,7 +67,7 @@ function App() {
             </motion.h1>
 
             <TextAnimate className="text-2xl" delay={1.5}>
-              Click above to learn more
+              Click above to learn more, if you want ¯\_(ツ)_/¯
             </TextAnimate>
           </motion.div>
         ) : (
@@ -74,10 +76,39 @@ function App() {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
+            transition={{ duration: 2.5 }}
             className="flex flex-col items-center gap-y-8"
           >
-            <p>temp</p>
+            <h1
+              className="text-3xl font-bold tracking-tighter md:text-3xl lg:text-5xl text-center flex justify-center"
+              onClick={() => setIsIntroVisible(false)}
+            >
+              {words.map((word, idx) => (
+                <motion.span
+                  layout
+                  key={idx}
+                  className="inline-block mr-1.5"
+                  variants={wordVariants}
+                  transition={{ duration: 0.5, ease: "easeIn" }}
+                >
+                  {word === "GPT" ? (
+                    <AuroraText>{word}</AuroraText>
+                  ) : (
+                    <p>{word}</p>
+                  )}
+                </motion.span>
+              ))}
+            </h1>
+            <div className="flex flex-row">
+              <Textarea
+                className="h-20 w-150 text-left resize-none scrollbar-thin scroll-smooth cursor-none"
+                placeholder="Ask me anything... this agent knows my resume better than I do"
+              />
+              <div className="flex flex-col ml-2 space-y-2">
+                <Button className="h-9 w-9 bg-blue-500 border border-blue-500 cursor-none transform transition-transform duration-100 hover:scale-110 hover:bg-blue-500"><Send size={24}/></Button>
+                <Button className="h-9 w-9 bg-gray-100 border border-gray-300 cursor-none transform transition-transform duration-100 hover:scale-110 hover:bg-gray-100"><LucideFile size={24} color="black"/></Button>
+              </div>
+            </div>
           </motion.div>
         )}
       </AnimatePresence>
---
+++ my-page/tailwind.config.js
@@ -0,0 +1,10 @@
+import tailwindScrollbar from 'tailwind-scrollbar';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  plugins: [tailwindScrollbar],
+};
