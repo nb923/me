fecf9329191ec3e165da14c1749cb3be5566aa79
nb923 <nb923@scarletmail.rutgers.edu>
2025-08-10 18:41:05 -0400

    Add interests text and vector db

--- backend/portfolio_mcp_server.py
+++ backend/portfolio_mcp_server.py
@@ -1,18 +1,50 @@
+
+import os
+from dotenv import load_dotenv
 from mcp.server.fastmcp import FastMCP
 from langchain_community.vectorstores import SKLearnVectorStore
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from tavily import TavilyClient
+
+load_dotenv()
 
 mcp = FastMCP("Portfolio Agent Tools")
 resume_db_path = "./resume_vector_db.parquet"
+interests_db_path = "./interests_vector_db.parquet"
 embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
-vector_db = SKLearnVectorStore(
+tavily_client = TavilyClient(os.getenv("TAVILY_API_KEY"))
+
+resume_vector_db = SKLearnVectorStore(
     embedding=embedding, persist_path=resume_db_path, serializer="parquet"
 )
-@mcp.tool()
+
+interests_vector_db = SKLearnVectorStore(
+    embedding=embedding, persist_path=interests_db_path, serializer="parquet"
+)
+
+loader = PyPDFLoader("./assets/nideesh-bk-resume.pdf", mode="single")
+docs = loader.load()
+
+@mcp.tool(description="Search Nideesh's resume for relevant sections matching the query string and return those snippets.")
 def query_resume(query: str):
-    results = vector_db.similarity_search(query)
+    results = resume_vector_db.similarity_search(query)
     return [result.page_content for result in results]
+
+@mcp.tool(description="Search Nideesh's interests text for relevant sections matching the query string and return those snippets.")
+def query_interests(query: str):
+    results = interests_vector_db.similarity_search(query)
+    return [result.page_content for result in results]
+
+@mcp.tool(description="Return the full content of Nideesh's resume as a single text string.")
+def resume_dump():
+    return docs[0].page_content
+
+@mcp.tool(description="Search the web for information that you may not known.")
+def search_web(query: str):
+    response = tavily_client.search(query=query)
+    return response["results"]
     
 
 if __name__ == "__main__":
--- backend/resume_vector_store.py
+++ backend/resume_vector_store.py
@@ -1,3 +1,4 @@
+from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
@@ -30,6 +31,26 @@ vector_db = SKLearnVectorStore.from_documents(texts, embedding=embedding, persis
 results = vector_db.similarity_search(
     "Which project involved OpenCV"
 )
+
+vector_db.persist()
+
+loader = TextLoader("./assets/interests.txt", encoding='utf-8')
+documents = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=300,
+    chunk_overlap=45,
+    separators=[
+        "\n\n",
+        "\n",
+        "---",
+    ],
+)
+
+texts = text_splitter.split_documents(documents)
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+vector_db_path = "./interests_vector_db.parquet"
+vector_db = SKLearnVectorStore.from_documents(texts, embedding=embedding, persist_path=vector_db_path, serializer="parquet")
 
 vector_db.persist()
 
